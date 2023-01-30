ee247895ff87234552fbfa81eaecf803e4723c83
ew11160 <ea11160@byu.edu>
2023-01-30 14:22:12 -0700

    Where is this going?

---
+++ FirstASPNETWebApp/FirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,14 @@
+﻿//// This is the javascript way
+//// document.getElementById("btnSend").addEventListener("click", function () {
+////    alert("From: " + document.getElementById("txtFrom").value +
+////        ", Subject: " + document.getElementById("txtSubject").value +
+////        ", Message: " + document.getElementById("txtMessage").value);
+////})
+
+//// this is the jquery way
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+    $("#picGanderson").fadeToggle;
+})
